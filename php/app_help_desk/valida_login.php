ca9645317ae2b14ba0ad978b0ac459b015135be9
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-12 18:01:38 -0300

    Métodos GET e POST PHP

---
+++ php/app_help_desk/valida_login.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    print_r($_GET);
+
+    echo "<br>";
+
+    echo $_GET["email"];
+    echo "<br>";
+    echo $_GET["senha"];
+    */
+
+    print_r($_POST);
+
+    echo "<br>";
+
+    echo $_POST["email"];
+    echo "<br>";
+    echo $_POST["senha"];
+?>
