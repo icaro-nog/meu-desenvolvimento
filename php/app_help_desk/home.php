fefe8bcf3d2257807645ff8d2fc34d1f1018d526
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-13 18:54:35 -0300

    Função para não permitir que usuário acesse páginas protegidas

--- php/app_help_desk/home.php
+++ php/app_help_desk/home.php
@@ -1,3 +1,15 @@
+<?php
+  /* Função para não permitir que usuário acesse páginas home.php, abrir_chamado.php e consultar_chamado.php */
+
+  session_start();
+
+  /* ! para verificar se NÃO existe a $_SESSION["autenticado"] */
+  /* Função nativa IS SET, verificar se está setado */
+  if(!isset($_SESSION["autenticado"]) || $_SESSION["autenticado"] != "SIM"){
+    header("Location: index.php?login=erro2");
+  }
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
