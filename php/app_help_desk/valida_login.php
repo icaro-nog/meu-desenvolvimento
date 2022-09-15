b7a4393b2c98642033b6f8eded9056c63144d820
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-15 17:49:58 -0300

    Criação de dois índices na variável  para testes

--- php/app_help_desk/valida_login.php
+++ php/app_help_desk/valida_login.php
@@ -31,6 +31,8 @@
     if($usuario_autenticado){
         echo "Usuário autenticado";
         $_SESSION["autenticado"] = "SIM";
+        $_SESSION["x"] = "um valor";
+        $_SESSION["y"] = "outro valor";
         header("Location: home.php");
     }else{
         $_SESSION["autenticado"] = "NAO";
