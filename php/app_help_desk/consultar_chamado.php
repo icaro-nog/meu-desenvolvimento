07208d9760b613c95595f871d3e39c5e9c82d112
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-15 17:51:30 -0300

    Inclusão de botão logoff.php

--- php/app_help_desk/consultar_chamado.php
+++ php/app_help_desk/consultar_chamado.php
@@ -26,6 +26,11 @@
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="logoff.php">SAIR</a>
+        </li>
+      </ul>
     </nav>
 
     <div class="container">    
