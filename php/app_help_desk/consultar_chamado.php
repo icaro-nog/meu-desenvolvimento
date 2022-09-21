6c66f3295557210bc324c1cf519007c5bd2b9882
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-21 12:45:02 -0300

    Correção de comentário require_once. Tag PHP para criação de array chamados, abrir registro_chamado.hd, função para percorrer o arquivo enquanto houver linhas, listar de forma dinâmica em arrays o arquivo registro_chamado.hd, fechar o arquivo registro_chamado.hd, imprimir o card no html a cada array %chamado criado, Se a quantidade de índices for menor que 3 em , não irá imprimir, Imprimir [] por índice

--- php/app_help_desk/consultar_chamado.php
+++ php/app_help_desk/consultar_chamado.php
@@ -1,6 +1,29 @@
 <?php
-  /* Passagem de script validador_acesso.php */
+  /* Passagem de script validador_acesso.php para proteger página, se login não tiver sido feito */
   require_once "validador_acesso.php";
+?>
+<?php
+
+  // Array de chamados
+  $chamados = [];
+
+  // Abrir registro_chamado.hd
+  $arquivo = fopen("registro_chamado.hd", "r");
+
+  // Percorrer o arquivo, enquanto houver registros(linhas) a serem recuperadas
+  // feof() -> Find End Of File -> Procura pelo fim do arquivo
+  // feof() -> Lê a primeira linha do arquivo, retorna false para a função pq não encontrou o final do arquivo, por isso está sendo usado "!". Após ler a primeira linha, passa para a segunda e assim segue...
+  while(!feof($arquivo)){
+    // fgets() -> Recupera o que estiver na linha de registro_chamado.hd até a quebra de linha
+    $registro = fgets($arquivo);
+    // Listar de forma dinâmica em arrays o registro_chamado.hd
+    $chamados[] = $registro;
+
+  }
+
+  // Fechar o registro_chamado.hd aberto
+  fclose($arquivo);
+
 ?>
 
 <html>
@@ -43,25 +66,33 @@
             </div>
             
             <div class="card-body">
+
+              <!-- Imprimir o card no html a cada array $chamado criado -->
+              <?php foreach($chamados as $chamado) { ?>
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                <?php 
+                  // Remover "#" dos arrays
+                  $chamado_dados = explode("#", $chamado);
 
+                  // Se a quantidade de índices for menor que 3 em $chamado_dados, não irá imprimir
+                  if(count($chamado_dados) < 3){
+                    continue;
+                  }
+                ?>
+                
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <!-- Imprimir $chamado_dados[] por índice -->
+                    <h5 class="card-title"> <?php echo $chamado_dados[0]; ?> </h5>
+                    <h6 class="card-subtitle mb-2 text-muted"> <?php echo $chamado_dados[1]; ?> </h6>
+                    <p class="card-text"> <?php echo $chamado_dados[2]; ?> </p>
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
-
+              <?php } ?>
+              <!-- Fim do foreach() PHP -->
+              
               <div class="row mt-5">
                 <div class="col-6">
                   <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
