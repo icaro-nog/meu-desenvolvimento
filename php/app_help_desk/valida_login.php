d4b8125e7fdf1f26a7e5dfb16453f6c52eb584d2
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-21 14:38:21 -0300

    Inclusão de perfis adm e user na $_SESSION

--- php/app_help_desk/valida_login.php
+++ php/app_help_desk/valida_login.php
@@ -4,12 +4,17 @@
 
     // Variável que verifica se a autenticação foi realizada
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = [1 => "Administrativo", 2 => "Usuário"];
 
     // Usuários do sistema
     $usuarios_app = [
-        ["email" => "adm@teste.com.br", "senha" => "123456"],
-        ["email" => "user1@teste.com.br", "senha" => "abcd"],
-
+        ["id" => 1, "email" => "adm@teste.com.br", "senha" => "123456", "perfil_id" => 1],
+        ["id" => 2, "email" => "user@teste.com.br", "senha" => "123456", "perfil_id" => 1],
+        ["id" => 3, "email" => "jose@teste.com.br", "senha" => "123456", "perfil_id" => 2],
+        ["id" => 4, "email" => "maria@teste.com.br", "senha" => "123456", "perfil_id" => 2],
     ];
 
     /*
@@ -25,14 +30,16 @@
        /* Autenticando usuário */
        if($user["email"] == $_POST["email"] && $user["senha"] == $_POST["senha"]){
             $usuario_autenticado = true;
+            $usuario_id = $user["id"];
+            $usuario_perfil_id = $user["perfil_id"];
        }
     }
 
     if($usuario_autenticado){
         echo "Usuário autenticado";
         $_SESSION["autenticado"] = "SIM";
-        $_SESSION["x"] = "um valor";
-        $_SESSION["y"] = "outro valor";
+        $_SESSION["id"] = $usuario_id;
+        $_SESSION["perfil_id"] = $usuario_perfil_id;
         header("Location: home.php");
     }else{
         $_SESSION["autenticado"] = "NAO";
