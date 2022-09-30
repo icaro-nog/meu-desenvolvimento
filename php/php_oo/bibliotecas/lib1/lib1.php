199c856794093f82dd2f7fe0f80707738e0a1617
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-30 15:16:14 -0300

    Lib1 com namespace A, requisitada em namespaces_parte2

---
+++ php/php_oo/bibliotecas/lib1/lib1.php
@@ -0,0 +1,35 @@
+<?php
+
+    namespace A;
+    class Cliente implements CadastroInterface{
+
+        public $nome = "Ícaro";
+
+        public function __construct(){
+
+            echo "<pre>";
+            print_r(get_class_methods($this));
+            echo "</pre>";
+        }
+
+        public function __get($attr){
+
+            return $this->$attr;
+        }
+        public function salvar(){
+
+            echo "Salvar";
+        }
+        public function remover(){
+
+            echo "Remover";
+        }
+    }
+
+    interface CadastroInterface{
+
+        public function salvar();
+    }
+
+
+?>    
