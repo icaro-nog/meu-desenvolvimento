69ed5e166e225ab7d4b9dff5a56aa987329a7b33
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-09-30 15:14:58 -0300

    dessa forma não gerando conflitos entre classe

--- php/php_oo/namespaces_parte2.php
+++ php/php_oo/namespaces_parte2.php
@@ -1,10 +1,11 @@
 <?php
 
+    // Requisitando lib1 e lib2
     require "./bibliotecas/lib1/lib1.php";
     require "./bibliotecas/lib2/lib2.php";
 
     // \...\ Para referenciar de qual lib será "retirada" a classe
-    // as -> Para atribuir a class a esse novo "nome", a instancia será feita a partir desse novo "nome"
+    // as -> Para atribuir a class a esse novo "nome", a instancia será feita a partir desse novo "nome", dessa forma não gerando conflitos entre classes
     use \A\Cliente as C1;
     use \B\Cliente as C2;
 
