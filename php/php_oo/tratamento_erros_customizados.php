4deb44a52ed783d33d8d7384323ab10bea141e3b
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-04 12:26:37 -0300

    Customizar erros com classes e criação de métodos

---
+++ php/php_oo/tratamento_erros_customizados.php
@@ -0,0 +1,35 @@
+<?php
+
+    class MinhaExceptionCustomizada extends Exception{
+
+        private $erro = "";
+
+        public function __construct($erro){
+
+            $this->erro = $erro;
+        }
+
+        public function exibirMensagemErroCustomizada(){
+
+            echo "<div style='border: 1px solid black; padding: 15px; color: white; background-color: red;'>";
+                echo $this->erro;
+            echo "</div>";
+        }
+    }
+
+    try{
+
+        // Lançando MinhaExceptionCustomizada para o Catch poder exibir
+        throw new MinhaExceptionCustomizada("Esse é um erro de teste");
+
+
+        // Error -> Utilizado mais para testes, não em aplicações reais
+        // Exception -> Utilizado em aplicações reais, por programadores
+        // Customizadas -> Utilizado em aplicações reais, por programadores, caso seja necessário customizar algum erro
+
+    }catch(MinhaExceptionCustomizada $e){
+        // o método exibirMensagemErroCustomizada pode ser acessado, por conta do extendes que há na class MinhaExceptionCustomizada
+        $e->exibirMensagemErroCustomizada();
+    }
+
+?>
