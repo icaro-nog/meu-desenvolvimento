94c8b25aa765fb21fa72cf63541c6e59210a9e5b
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-04 12:21:52 -0300

    Correção de comentário

--- php/app_help_desk/valida_login.php
+++ php/app_help_desk/valida_login.php
@@ -1,4 +1,4 @@
 <?php
-    // Script para proteger o valida_login.php -> Caminho feito C:\xampp\htdocs\meu-desenvolvimento\php\app_help_desk para se chegar a pasta xamp
+    // Script para proteger o valida_login.php -> Caminho feito C:\xampp\htdocs\app_help_desk para se chegar a pasta app_help_desk
     require "../../../../app_help_desk/valida_login.php"
 ?>
