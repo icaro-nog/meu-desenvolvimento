19c06a873cb53944db8496c5b82204026d82ea73
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-07 11:11:32 -0300

    Comentário adicionado sobre diretório htdocs por segurança

--- php/app_help_desk/valida_login.php
+++ php/app_help_desk/valida_login.php
@@ -1,4 +1,5 @@
 <?php
     // Script para proteger o valida_login.php -> Caminho feito C:\xampp\htdocs\app_help_desk para se chegar a pasta app_help_desk
+    // Pasta app_help_desk/valida_login.php foi criada fora do diretório htdocs por questões de segurança, protegendo os usuários e senhas do sistema
     require "../../../../app_help_desk/valida_login.php"
 ?>
