9e73e6018deb49b4af00a2eb47a1837fe16ac40d
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-07 11:26:39 -0300

    Aprimoramento de comentário sobre diretório htdocs

--- php/app_help_desk/valida_login.php
+++ php/app_help_desk/valida_login.php
@@ -1,5 +1,5 @@
 <?php
-    // Script para proteger o valida_login.php -> Caminho feito C:\xampp\htdocs\app_help_desk para se chegar a pasta app_help_desk
-    // Pasta app_help_desk/valida_login.php foi criada fora do diretório htdocs por questões de segurança, protegendo os usuários e senhas do sistema
+    // Script para proteger o valida_login.php -> Caminho feito C:\xampp\htdocs\app_help_desk para se chegar ao arquivo valida_login.php
+    // Pasta app_help_desk/valida_login.php foi transferida para fora do diretório htdocs por questões de segurança, protegendo os usuários e senhas do sistema
     require "../../../../app_help_desk/valida_login.php"
 ?>
