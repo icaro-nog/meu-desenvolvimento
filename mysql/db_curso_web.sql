2fb9983e47b6f48b8b0e921d3187e83308b9b565
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-11 18:02:59 -0300

    Criação de uma nova tabela no banco de dados

--- mysql/db_curso_web.sql
+++ mysql/db_curso_web.sql
@@ -3,3 +3,19 @@ CREATE DATABASE db_curso_web;
 
 /* Para exclusão de banco/base de dados */
 /* DROP db_curso_web; */
+
+/* Para criação de uma nova tabela no banco de dados */
+CREATE TABLE tb_cursos (
+    /* not null, preenchimento obrigatório */
+	id_curso int not null,
+    imagem_curso varchar(100) not null,
+    nome_curso char(50) not null,
+    /* null, preenchimento opcional */
+    resumo text null,
+    data_cadastro datetime not null,
+    ativo boolean default true,
+    investimento float(5, 2) default 0
+);
+
+/* Para exclusão de tabela do banco de dados */
+/* DROP TABLE tb_cursos; */
