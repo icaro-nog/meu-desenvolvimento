151587e1c661cf9a61d62f5c584792c21f04cbf8
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-12 14:13:05 -0300

    Renomear tabela

--- mysql/db_curso_web.sql
+++ mysql/db_curso_web.sql
@@ -1,10 +1,10 @@
-/* Para criação de banco/base de dados */
+/* Criação de banco/base de dados */
 CREATE DATABASE db_curso_web;
 
-/* Para exclusão de banco/base de dados */
-/* DROP db_curso_web; */
+/* Exclusão de banco/base de dados */
+DROP db_curso_web;
 
-/* Para criação de uma nova tabela no banco de dados */
+/* Criação de uma nova tabela no banco de dados */
 CREATE TABLE tb_cursos (
     /* not null, preenchimento obrigatório */
 	id_curso int not null,
@@ -17,8 +17,11 @@ CREATE TABLE tb_cursos (
     investimento float(5, 2) default 0
 );
 
-/* Para exclusão de tabela do banco de dados */
-/* DROP TABLE tb_cursos; */
+/* Exclusão de tabela do banco de dados */
+DROP TABLE tb_cursos;
 
 /* CHAR - aloca um tamanho fixo no disco, se foi definido que será de 10 posições, é alocado 10 posições para o recebimento de dados. Vantagem: Mais rápido para pesquisas - Desvantagem: se mau utilizado, pode pesar o disco de armazenamento */
 /* VARCHAR - aloca um tamanho variável no disco, se foi definido como 10 posições, mas o dado contem 4 posições, ele apenas utilizará 4 posições. Vantagem: Ocupa menos espaço em disco - Desvantagem: maior lentidão em pesquisas */
+
+/* Renomear tabela */
+RENAME TABLE 'nome_atual' TO 'nome_novo';
