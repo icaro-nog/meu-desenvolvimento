ecca809ca05e6a328bee6b037a757facdea53224
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-17 11:02:10 -0300

    Querys necessárias para funções de agregação

--- mysql/db_curso_web.sql
+++ mysql/db_curso_web.sql
@@ -115,6 +115,11 @@ SELECT * FROM `tb_alunos` LIMIT 4 OFFSET 0;
 -- partindo do id 8, listagem de 4 registros (forma mais enxuta)
 SELECT * FROM `tb_alunos` LIMIT 8, 4;
 
+/* Querys necessárias para funções de agregação */
+TRUNCATE tb_cursos;
+
+INSERT INTO tb_cursos(id_curso, imagem_curso, nome_curso, resumo, data_cadastro, ativo, investimento, carga_horaria) VALUES (1, 'curso_node.jpg', 'Curso Completo do Desenvolvedor NodeJS e MongoDB', 'Resumo do curso de NodeJS', '2018-01-01', 1, 159.99, 15), (2, 'curso_react_native.jpg', 'Multiplataforma Android/IOS com React e Redux', 'Resumo do curso de React Native', '2018-02-01', 1, 204.99, 37), (3, 'angular.jpg', 'Desenvolvimento WEB com ES6, TypeScript e Angular', 'Resumo do curso de ES6, TypeScript e Angular', '2018-03-01', 1, 579.99, 31), (4, 'web_completo_2.jpg', 'Web Completo 2.0', 'Resumo do curso de Web Completo 2.0', '2018-04-01', 1, 579.99, 59), (5, 'linux.jpg', 'Introdução ao GNU/Linux', 'Resumo do curso de GNU/Linux', '2018-05-01', 0, 0, 1)
+
 /* Funções de agregação parte 1: MIN, MAX e AVG */
 -- Retorna o investimento MÍNIMO de tb_cursos
 SELECT MIN(investimento) FROM `tb_cursos`;
