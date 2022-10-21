47178b832695ed826bbfe24a6e31e2643664dc13
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-21 12:07:15 -0300

    Exercícios finalizados

--- mysql/exercícios_para_fixacao.sql
+++ mysql/exercícios_para_fixacao.sql
@@ -24,18 +24,23 @@ ALTER TABLE `tb_clientes` ADD `endereco` VARCHAR(150) NULL DEFAULT NULL ;
 para “M”. Utilize a instrução IN para este fim.
 */
 -- R:
-
+-- Insert de registros
+INSERT INTO tb_clientes (nome, idade, sexo, endereco) VALUES ("Marcos", 21, "F", "Restinga, Porto Alegre"), ("Gustavo", 25, "M", "Centro, Porto Alegre"), ("Márcio", 49, "F", "Olaria, Porto Alegre"), ("Márcio", 49, "M", "Bairro 3, Porto Alegre"), ("Fernanda", 49, "F", "Outro bairro, Porto Alegre"), ("William", 49, "M", "Capital, Porto Alegre");
+-- Update de sexo
+UPDATE tb_clientes SET sexo = "M" WHERE id_cliente IN(1, 2, 3, 6, 7);
 
 /*
 4) Efetue um update em tb_clientes dos registros de id_cliente igual a 4, 5, 8, 9 e 10, atualizando o sexo
 para “F”. Como desafio, faça este update utilizando dois between’s no filtro.
 */
 -- R:
+UPDATE tb_clientes SET sexo = "F" WHERE id_cliente BETWEEN 4 AND 5 AND id_cliente BETWEEN 8 AND 10;
 
 /*
 5) Selecione todos os registros de tb_clientes que possuam relação com tb_pedidos e com tb_pedidos
-produtos (apenas registros com relacionamentos entre si). Recupe também os detalhes dos produtos da
+produtos (apenas registros com relacionamentos entre si). Recupere também os detalhes dos produtos da
 tabela tb_produtos. A consulta deve retornar de tb_clientes as colunas “id_cliente”, “nome”, “idade” e
 de tb_produtos deve ser retornado as colunas “produto” e “valor”.
 */
 -- R:
+SELECT clientes.id_cliente, clientes.nome, clientes.idade, produtos.produto, produtos.valor FROM tb_produtos AS produtos, tb_clientes AS clientes INNER JOIN tb_pedidos ON(clientes.id_cliente = tb_pedidos.id_cliente) INNER JOIN tb_pedidos_produtos ON(tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido );
