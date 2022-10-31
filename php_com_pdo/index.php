c408ed26c6b8ed6e384c63cad3c1e452c4c04c1e
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-10-31 17:50:04 -0300

    SQL Exec

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -1,11 +1,30 @@
 <?php
 
-    $dsn = "mysql:host=localhost; dbname=xphp_com_pdo";
+    $dsn = "mysql:host=localhost; dbname=php_com_pdo";
     $usuario = "root";
     $senha = "";
 
     try{
         $conexao = new PDO($dsn, $usuario, $senha);
+
+        $query = "
+            create table if not exists tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+        ";
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
+
+        $query = "
+            delete from tb_usuarios
+        ";
+
+        $retorno = $conexao->exec($query);
+        echo $retorno;
         
     }catch(PDOException $e){
         
