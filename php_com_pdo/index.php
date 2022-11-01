6e214e6989a62c7e796af8fb9446885b11cd7998
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-11-01 09:15:39 -0300

    Fetch - Retornando apenas um registro da lista de arrays

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -14,13 +14,17 @@
         $stmt = $conexao->query($query);
         /* fetch(): Retorna uma unica row da consulta, ideal para poder utilizar em consultas como login, que retorna somente um resultado.
         fetchAll(): Retorna um array com todas as linhas da consulta, ideal para uma busca por nome ou por endereço. */
-        $lista = $stmt->fetchAll();
+        // PDO::FETCH_NUM - Retorna apenas índices númericos do array
+        // PDO::FETCH_ASSOC - Retorna apenas índices associativos do array
+        // PDO::FETCH_BOTH - Retorna ambos
+        // PDO::FETCH_OBJ - Retorna o array em formato de objeto, sendo necessário acessá-lo com ->
+        $lista = $stmt->fetch(PDO::FETCH_OBJ);
 
         echo "<pre>";
-        print_r($lista[0]["nome"]);
+        print_r($lista);
         echo "</pre>";
         // Retornando índices do array de fetchAll
-        echo $lista[2][2];
+        //echo $lista[6]->nome;
 
     }catch(PDOException $e){
         
