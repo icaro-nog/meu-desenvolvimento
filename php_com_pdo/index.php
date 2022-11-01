ff34f5b1706a46b949eff60e29de9f954193e01b
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-11-01 09:01:33 -0300

    PDOStatement com Query FetchAll

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -8,24 +8,20 @@
         $conexao = new PDO($dsn, $usuario, $senha);
 
         $query = "
-            create table if not exists tb_usuarios(
-                id int not null primary key auto_increment,
-                nome varchar(50) not null,
-                email varchar(100) not null,
-                senha varchar(32) not null
-            )
+            select * from tb_usuarios
         ";
 
-        $retorno = $conexao->exec($query);
-        echo $retorno;
+        $stmt = $conexao->query($query);
+        /* fetch(): Retorna uma unica row da consulta, ideal para poder utilizar em consultas como login, que retorna somente um resultado.
+        fetchAll(): Retorna um array com todas as linhas da consulta, ideal para uma busca por nome ou por endereço. */
+        $lista = $stmt->fetchAll();
 
-        $query = "
-            delete from tb_usuarios
-        ";
+        echo "<pre>";
+        print_r($lista[0]["nome"]);
+        echo "</pre>";
+        // Retornando índices do array de fetchAll
+        echo $lista[2][2];
 
-        $retorno = $conexao->exec($query);
-        echo $retorno;
-        
     }catch(PDOException $e){
         
         echo "Erro: ".$e->getCode(). " Mensagem: ".$e->getMessage();
