a62cb57a5ff25df7ed6d4f74847648664fe74e2d
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-11-07 12:04:24 -0300

    Atualizando registros parte de front-end com javascript

--- php_com_pdo/app_lista_tarefas_public/todas_tarefas.php
+++ php_com_pdo/app_lista_tarefas_public/todas_tarefas.php
@@ -21,6 +21,61 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+
+			function editar(id, txt_tarefa){
+				
+				// Criar um formulário de edição
+				let form = document.createElement("form")
+				form.action = "#"
+				form.method = "post"
+				form.className = "row"
+
+				// Criar um input para entrada
+				let inputTarefa = document.createElement("input")
+				inputTarefa.type = "text"
+				inputTarefa.name = "tarefa"
+				inputTarefa.className = "col-9 form-control"
+				inputTarefa.value = txt_tarefa
+
+				// Criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement("input")
+				inputId.type = "hidden"
+				inputId.name = "id"
+				// Definindo id que foi passado por parâmetro a tag <script>
+				inputId.value = id
+
+				// Criar um button para envio do formulário
+				let button = document.createElement("form")
+				button.type = "submit"
+				button.className = "col-3 btn btn-info"
+				button.innerHTML = "Atualizar"
+
+				// Inclusão de inputTarefa no formulário
+				form.appendChild(inputTarefa)
+
+				// Incluir inputId no formulário
+				form.appendChild(inputId)
+
+				// Incluir button no formulário
+				form.appendChild(button)
+
+				// Teste
+				//console.log(form)
+
+				// Selecionar div tarefa pelo id recebido por parâmetro
+				let tarefa = document.getElementById("tarefa_" + id)
+
+				// Limpar texto da tarefa para inclusão do form
+				tarefa.innerHTML = ""
+
+				// Inclusão do form na página
+				// O insertBefore() método insere um nó filho antes de um filho existente.
+				tarefa.insertBefore(form, tarefa[0])
+			}
+
+		</script>
 	</head>
 
 	<body>
@@ -54,10 +109,13 @@
 								<?php foreach($tarefas as $indice => $tarefa){ ?>
 
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?php echo $tarefa->tarefa ?> (<?php echo $tarefa->status ?>)</div>
+										<!-- id da tag de acordo com o id do banco de dados -->
+										<div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
+											<?php echo $tarefa->tarefa ?> (<?php echo $tarefa->status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
