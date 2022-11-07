bfdaec300561625bb9ad58016963510a415d41d9
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-11-07 11:14:25 -0300

    Listando todas as tarefas com foreach no array $tarefas

--- php_com_pdo/app_lista_tarefas_public/todas_tarefas.php
+++ php_com_pdo/app_lista_tarefas_public/todas_tarefas.php
@@ -4,6 +4,11 @@
 	$acao = "recuperar";
 	require "tarefa_controller.php";
 
+	/*
+	echo "<pre>";
+	print_r($tarefas);
+	echo "</pre>";
+	*/
 ?>
 
 
@@ -45,23 +50,19 @@
 								<h4>Todas tarefas</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<!-- Foreach para percorrer o array $tarefas vindo de require "tarefa_controller.php" -->
+								<?php foreach($tarefas as $indice => $tarefa){ ?>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9"><?php echo $tarefa->tarefa ?> (<?php echo $tarefa->status ?>)</div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-check-square fa-lg text-success"></i>
+										</div>
 									</div>
-								</div>
+								
+								<?php } ?>
 								
 							</div>
 						</div>
