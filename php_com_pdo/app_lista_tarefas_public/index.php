dc64a4d94b2f0fc6ea94a2c8d4b853e84d99eefc
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2022-12-01 15:44:57 -0300

    Funções de editar, remover e marcar_realizada adicionadas, e listagem dinâmica de tarefas pendentes

--- php_com_pdo/app_lista_tarefas_public/index.php
+++ php_com_pdo/app_lista_tarefas_public/index.php
@@ -1,3 +1,17 @@
+<?php
+
+	// Se clicar em Todas as Tarefas, a $acao será setada como "recuperar"
+	$acao = "pendentes";
+	require "tarefa_controller.php";
+
+	/*
+	echo "<pre>";
+	print_r($tarefas_pendentes);
+	echo "</pre>";
+	*/
+	
+?>
+
 <html>
 	<head>
 		<meta charset="utf-8" />
@@ -7,6 +21,71 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+
+			function editar(id, txt_tarefa){
+				
+				// Criar um formulário de edição
+				let form = document.createElement("form")
+				form.action = "index.php?pag=index&acao=atualizar"
+				form.method = "post"
+				form.className = "row"
+
+				// Criar um input para entrada
+				let inputTarefa = document.createElement("input")
+				inputTarefa.type = "text"
+				inputTarefa.name = "tarefa"
+				inputTarefa.className = "col-9 form-control"
+				inputTarefa.value = txt_tarefa
+
+				// Criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement("input")
+				inputId.type = "hidden"
+				inputId.name = "id"
+				// Definindo id que foi passado por parâmetro a tag <script>
+				inputId.value = id
+
+				// Criar um button para envio do formulário
+				let button = document.createElement("button")
+				button.type = "submit"
+				button.className = "col-3 btn btn-info"
+				button.innerHTML = "Atualizar"
+
+				// Inclusão de inputTarefa no formulário
+				form.appendChild(inputTarefa)
+
+				// Incluir inputId no formulário
+				form.appendChild(inputId)
+
+				// Incluir button no formulário
+				form.appendChild(button)
+
+				// Teste
+				//console.log(form)
+
+				// Selecionar div tarefa pelo id recebido por parâmetro
+				let tarefa = document.getElementById("tarefa_" + id)
+
+				// Limpar texto da tarefa para inclusão do form
+				tarefa.innerHTML = ""
+
+				// Inclusão do form na página
+				// O insertBefore() método insere um nó filho antes de um filho existente.
+				tarefa.insertBefore(form, tarefa[0])
+			}
+
+			function remover(id){
+				// Parâmetro pag = index para ser redirecionado a página index.php após function
+				location.href = "index.php?pag=index&acao=remover&id="+id;
+			}
+
+			function marcar_realizada(id){
+				// Parâmetro pag = index para ser redirecionado a página index.php após function
+				location.href = "index.php?pag=index&acao=marcarRealizada&id="+id;
+			}
+		</script>
+
 	</head>
 
 	<body>
@@ -36,23 +115,26 @@
 								<h4>Tarefas pendentes</h4>
 								<hr />
 
+								<!-- Foreach para percorrer o array $tarefas_pendentes vindo de require "tarefa_controller.php" -->
+								<?php foreach($tarefas_pendentes as $indice => $tarefa){ ?>
+
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro</div>
+									<!-- id da tag de acordo com o id do banco de dados -->
+									<div class="col-sm-9" id="tarefa_<?php echo $tarefa->id ?>">
+										<?php echo $tarefa->tarefa ?>
+									</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+										<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?php echo $tarefa->id ?>)"></i>
+
+											<!-- editar(id, txt_tarefa) inteiro e string -->
+										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?php echo $tarefa->id ?>, '<?php echo $tarefa->tarefa ?>')"></i>
+										<i class="fas fa-check-square fa-lg text-success" onclick="marcar_realizada(<?php echo $tarefa->id ?>)"></i>
+																			
 									</div>
 								</div>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php } ?>
+
 							</div>
 						</div>
 					</div>
