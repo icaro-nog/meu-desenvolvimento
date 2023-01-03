53e69f761957928cf7dba5edf43994d388dfd5e7
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-03 08:31:29 -0300

    função content para exibição de conteúdo e render para carregamento de conteúdo de acordo com o layout

--- mvc/projetos/miniframework/vendor/MF/Controller/Action.php
+++ mvc/projetos/miniframework/vendor/MF/Controller/Action.php
@@ -7,10 +7,27 @@
         protected $view;
 
         public function __construct() {
+            // stdClass para tornar essa $view em uma classe padrão de objetos do PHP
             $this->view = new \stdClass();
         }
 
-        protected function render($view) {
+        // parâmetros $view e $layout de render estão vindo de IndexController.php 
+        protected function render($view, $layout) {
+
+            // Ajustar o contexto do parâmetro $view
+            $this->view->page = $view;
+
+            // Se o arquivo solicitado existir, exibir ele na página, senão, exibir apenas o conteúdo que já está carregado nela
+            if(file_exists("../App/Views/".$layout.".phtml")){
+                require_once "../App/Views/".$layout.".phtml";
+            }else{
+                $this->content();
+            }
+
+            
+        }
+
+        protected function content() {
 
             $classAtual = get_class($this);
 
@@ -18,9 +35,9 @@
 
             $classAtual = strToLower(str_replace("Controller", "", $classAtual));
 
-            echo $classAtual;
+            //echo $classAtual;
 
-            require_once "../App/Views/".$classAtual."/".$view.".phtml";
+            require_once "../App/Views/".$classAtual."/".$this->view->page.".phtml";
         }
 
     }
