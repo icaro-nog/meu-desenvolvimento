ae2ea9441bf1fe413b59aec35681d3f164368009
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-04 17:57:54 -0300

    Função para receber dados do formulário e realizar operações de verificação e registro

--- mvc/projetos/twitter_clone/App/Controllers/IndexController.php
+++ mvc/projetos/twitter_clone/App/Controllers/IndexController.php
@@ -18,6 +18,48 @@ class IndexController extends Action {
 		$this->render('inscreverse');
 	}
 
+	public function registrar() {
+
+		// Receber dados do formulário e salvá-los
+		$usuario = Container::getModel("Usuario");
+
+		$usuario->__set("nome", $_POST["nome"]);
+		$usuario->__set("email", $_POST["email"]);
+		$usuario->__set("senha", $_POST["senha"]);
+
+		if($usuario->validarCadastro()){
+			if(count($usuario->getUsuarioPorEmail()) == 0){
+				$usuario->salvar();
+				/*
+				echo '<script type="text/javascript">';
+				echo 'alert("Cadastro realizado com sucesso!");';  //not showing an alert box.
+				echo 'location.href="/";';
+				echo '</script>';
+				*/
+
+				$this->render("cadastro");
+			}
+
+			echo '<script type="text/javascript">';
+			echo 'alert("E-mail já cadastrado!");';  //not showing an alert box.
+			echo 'location.href="/";';
+			echo '</script>';	
+
+		}else{
+			echo '<script type="text/javascript">';
+			echo 'alert("Cadastro não realizado!");';  //not showing an alert box.
+			echo 'location.href="/";';
+			echo '</script>';
+		}
+		
+
+		// Sucesso 
+
+		// Erro
+
+
+	}
+
 }
 
 
