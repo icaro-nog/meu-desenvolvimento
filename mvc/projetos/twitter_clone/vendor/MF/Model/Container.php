46cd07d45885cf0174c47191b4f026259fa2e7ae
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-04 18:01:07 -0300

    Comentário complemetar

--- mvc/projetos/twitter_clone/vendor/MF/Model/Container.php
+++ mvc/projetos/twitter_clone/vendor/MF/Model/Container.php
@@ -4,6 +4,7 @@ namespace MF\Model;
 
 use App\Connection;
 
+// Responsável por instanciar um modelo de dado do banco de dados e juntamente instanciar uma conexão com o banco de dados
 class Container {
 
 	public static function getModel($model) {
