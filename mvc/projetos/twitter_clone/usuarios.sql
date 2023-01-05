b1c69af9da1061180f43ddf651475d6d6ab6929f
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-05 17:56:44 -0300

    Querys para o banco de dados

--- mvc/projetos/twitter_clone/usuarios.sql
+++ mvc/projetos/twitter_clone/usuarios.sql
@@ -1,6 +1,18 @@
+/* Criação de tabela de usuários */
 create table usuarios(
     id int not null primary key auto_increment,
     nome varchar(100) not null,
     email varchar(150) not null,
     senha varchar(32) not null
+)
+
+/* Transformando senhas em hashes de 32 caracteres */
+UPDATE usuarios SET senha = md5(senha) WHERE id IN(1, 2);
+
+/* Criação de tabela para tweets */
+CREATE TABLE tweets(
+	id int not null PRIMARY KEY AUTO_INCREMENT,
+    if_usuario int not null,
+    tweet varchar(140) not null,
+    data datetime DEFAULT CURRENT_TIMESTAMP
 )
