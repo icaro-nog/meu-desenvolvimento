a496e7c184c070f6d7b5c3d131d196a10be36ee4
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-09 12:55:12 -0300

    Função contagemTweets, acao e removerTweet

--- mvc/projetos/twitter_clone/App/Controllers/AppController.php
+++ mvc/projetos/twitter_clone/App/Controllers/AppController.php
@@ -24,15 +24,7 @@ class AppController extends Action {
 
         $this->view->tweets = $tweets;
 
-        // Contar tweets
-        $contagemTweets = count($tweet->getTweets());
-        $this->view->contagemTweets = $contagemTweets;
-
-        /*
-        echo "<pre>";
-        print_r($this->view->tweets);
-        echo "</pre>";
-        */
+        $this->contagemTweets();
 
         $this->render("timeline");
         /*
@@ -41,8 +33,6 @@ class AppController extends Action {
         print_r($_SESSION);
         echo "</pre>";
         */
-
-        
 
     }
 
@@ -86,6 +76,7 @@ class AppController extends Action {
 
             $usuario = Container::getModel("Usuario");
             $usuario->__set("nome", $pesquisarPor);
+            $usuario->__set("id", $_SESSION["id"]);
 
             $usuarios = $usuario->getUsuarios();
 
@@ -98,7 +89,68 @@ class AppController extends Action {
 
         $this->view->usuarios = $usuarios;
 
+        $this->contagemTweets();
+
         $this->render("quemSeguir");
+    }
+
+    // Contar tweets por usuario
+    public function contagemTweets() {
+
+        // Instanciar modelo de Tweet e conexão com o banco de dados
+        $tweet = Container::getModel("Tweet");
+
+        // Setando o id_usuario em getTweets()
+        $tweet->__set("id_usuario", $_SESSION["id"]);
+        
+        $tweet->getTweets();
+
+        // Contar tweets
+        $contagemTweets = count($tweet->getTweets());
+        $this->view->contagemTweets = $contagemTweets;
+
+    }
+
+    // Seguir ou deixar de seguir usuário, métodos feitos em Models/Usuario.php
+    public function acao() {
+
+        // Só irá executar o restante do código se o usuário for válido
+        $this->validaAutenticacao();
+
+        $acao = isset($_GET["acao"]) ? $_GET["acao"] : "";
+        $id_usuario_seguindo = isset($_GET["id"]) ? $_GET["id"] : "";
+
+        $usuario = Container::getModel("Usuario");
+        $usuario->__set("id", $_SESSION["id"]);
+
+        if($acao == "seguir"){
+            $usuario->seguirUsuario($id_usuario_seguindo);
+
+        }else if($acao == "deixar_de_seguir"){
+            $usuario->deixarSeguir($id_usuario_seguindo);
+
+        }
+
+        header("location: /quem_seguir");
+
+    }
+    
+    // Remover tweet 
+    public function removerTweet() {
+
+        // Só irá executar o restante do código se o usuário for válido
+        $this->validaAutenticacao();
+
+        // Instanciar modelo de Tweet e conexão com o banco de dados
+        $tweet = Container::getModel("Tweet");
+
+        // Setando o id_usuario em getTweets()
+        $tweet->__set("id", $_GET["id"]);
+        
+        $tweet->remover();
+
+        header("location: /timeline");
+
     }
 
     // Validar autenticação do usuário 
