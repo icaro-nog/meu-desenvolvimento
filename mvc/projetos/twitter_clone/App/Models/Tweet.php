fa124dbf09ec798ebe57caddeea59a9349d4d150
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-11 12:27:23 -0300

    contarTweets

--- mvc/projetos/twitter_clone/App/Models/Tweet.php
+++ mvc/projetos/twitter_clone/App/Models/Tweet.php
@@ -23,7 +23,7 @@
         // Método para salvar tweet
         public function salvar(){
 
-            $query = "insert into tweets(id_usuario, tweet) values(:id_usuario, :tweet)";
+            $query = "INSERT INTO tweets(id_usuario, tweet) VALUES(:id_usuario, :tweet)";
             // $this->db está vindo da extensão da classe Model
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(":id_usuario", $this->__get("id_usuario"));
@@ -42,21 +42,21 @@
             // DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') método mysql para formatar data
             // or
             // t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario;) para listagem de tweets de outros usuários
-            $query = "select 
+            $query = "SELECT 
                 t.id, 
                 t.id_usuario, 
                 u.nome, 
                 t.tweet, 
-                DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data 
-             from 
-                tweets as t
-                left join usuarios as u on (t.id_usuario = u.id)
-            where 
+                DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') AS data 
+             FROM 
+                tweets AS t
+                LEFT JOIN usuarios AS u ON (t.id_usuario = u.id)
+            WHERE 
                 t.id_usuario = :id_usuario
-                or
-                t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario) 
-            order by 
-                t.data desc";
+                OR
+                t.id_usuario IN (SELECT id_usuario_seguindo FROM usuarios_seguidores WHERE id_usuario = :id_usuario) 
+            ORDER BY 
+                t.data DESC";
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(":id_usuario", $this->__get("id_usuario"));
             $stmt->execute();
@@ -64,10 +64,21 @@
             return $stmt->fetchAll(\PDO::FETCH_ASSOC);
         }
 
+        // Método para contar tweets
+        public function contarTweets(){
+            // Contagem de acordo com o id da sessão
+            $query = "SELECT COUNT(*) AS TweetsUsuario FROM tweets WHERE id_usuario = :id_usuario";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(":id_usuario", $this->__get("id_usuario"));
+            $stmt->execute();
+
+            return $stmt->fetch(\PDO::FETCH_ASSOC);
+        }
+
         // Método para remover tweet
         public function remover(){
 
-            $query = "delete from tweets where id = :id";
+            $query = "DELETE FROM tweets WHERE id = :id";
             // $this->db está vindo da extensão da classe Model
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(":id", $this->__get("id"));
