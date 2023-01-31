4d1c536940119f4d0c9d938d83e03f4ec531739d
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-01-31 16:20:20 -0300

    Settings

---
+++ slim/slim-api/src/settings.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+
+        // DB Settings
+        'db' =>    [
+            'driver' => 'mysql',// Pode-se utilizar outros bancos de dados (Postgres, SQL Server, and SQLite)
+            'host' => 'localhost',
+            'database' => 'slim',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ]
+    ],
+];
