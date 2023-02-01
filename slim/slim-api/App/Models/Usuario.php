b6cb58d4f9be78a3384a1a9596cb1069fdd53a08
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-02-01 17:22:47 -0300

    Model Usuário

---
+++ slim/slim-api/App/Models/Usuario.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+O ideal é sempre nomear as classes no SINGULAR, pois o Eloquent já faz a correção para o PLURAL. Ex.:
+ROTA                ====      CLASSE
+produtos.php        ====      Produto
+carrinho_compras    ====      CarrinhoCompra
+...
+*/
+class Usuario extends Model{
+
+    
+}
