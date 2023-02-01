3e336b0b7a505715b0c4a9820ed96108435e6fe7
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-02-01 17:23:36 -0300

    Criação de middleware com Jwt

--- slim/slim-api/src/middleware.php
+++ slim/slim-api/src/middleware.php
@@ -1,4 +1,21 @@
 <?php
 // Application middleware
+//require '../../vendor/autoload.php';
+$app = new Slim\App;
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    "header" => "X-Token",
+    "regexp" => "/(.*)/",
+    "path" => "/api", /* or ["/api", "/admin"] */
+    "ignore" => ["/api/token"],
+    "secret" => $container->get("settings")["secretKey"]
+]));
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
