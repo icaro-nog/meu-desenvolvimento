283628325bf9036a18084604ecf6c5023b8655cd
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-02-08 16:37:07 -0300

    Data Binding

--- ionic/ionicApp/src/app/home/home.page.ts
+++ ionic/ionicApp/src/app/home/home.page.ts
@@ -9,4 +9,16 @@ export class HomePage {
 
   constructor() {}
 
+  public titulo: String = "Meu primeiro App"
+  public imagemRandomica: String = "https://source.unsplash.com/random/100x100"
+  public imagemLocal: String = "../assets/icone-celular.png"
+
+  public atualiza(): void {
+    this.titulo = "Texto alterado"
+  }
+
+  public acao(): void {
+    this.titulo = "Botão clicado"
+  }
+
 }
