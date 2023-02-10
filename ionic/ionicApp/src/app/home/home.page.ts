7282ccaec71e57bf4ee7311b9c0c37d41c8739d6
Ícaro Nogueira <icarolemosleal@outlook.com.br>
2023-02-10 19:34:11 -0300

    Importação NavControler e métodos

--- ionic/ionicApp/src/app/home/home.page.ts
+++ ionic/ionicApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private navegacao: NavController) {}
 
+  public pesquisa: String = ""
+  public resultado: String = ""
   public titulo: String = "Meu primeiro App"
   public imagemRandomica: String = "https://source.unsplash.com/random/100x100"
   public imagemLocal: String = "../assets/icone-celular.png"
+
+  recuperar() {
+    this.resultado = this.pesquisa
+  }
+
+  abrirBotoes() {
+    this.navegacao.navigateForward('botoes')
+  }
+
+  abrirLista() {
+    this.navegacao.navigateForward('lista')
+  }
+  
 
   public atualiza(): void {
     this.titulo = "Texto alterado"
