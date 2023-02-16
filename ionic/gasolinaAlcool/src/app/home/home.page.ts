26b1a76d89001376228111ce307fd2eb31c1005a
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-02-16 16:24:16 -0300

    Finalização de App Gasolina Álcool

--- ionic/gasolinaAlcool/src/app/home/home.page.ts
+++ ionic/gasolinaAlcool/src/app/home/home.page.ts
@@ -9,4 +9,28 @@ export class HomePage {
 
   constructor() {}
 
+  public resultado: string = "Resultado"
+  public precoAlcool: string = ""
+  public precoGasolina: string = ""
+
+  calcular(){
+
+    // Validar se os campos foram preenchidos
+    if( this.precoAlcool && this.precoGasolina ){
+      var pAlcool = parseFloat(this.precoGasolina)
+      var pGasolina = parseFloat(this.precoAlcool)
+
+      var res = pAlcool/pGasolina
+      if(res >= 0.7){
+        this.resultado = "Melhor utilizar Gasolina"
+      }else{
+        this.resultado = "Melhor utilizar Álcool"
+      }
+      
+    }else{
+      this.resultado = "Não preenchido"
+    }
+
+  }
+
 }
