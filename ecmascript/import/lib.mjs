79e6b8a1792ff15a6ad7a140cd9df6599841f18f
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-03-01 10:54:32 -0300

    Importação de forma dinâmica

--- ecmascript/import/lib.mjs
+++ ecmascript/import/lib.mjs
@@ -3,9 +3,3 @@ export const saudacao = () => {
     let data = new Date().toLocaleDateString()
     return `Olá, seja muito bem-vindo. Hoje é ${data}`
 }
-
-export function getEnderecoByCEP(cep) {
-
-    return fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => response.json())
-}
---
+++ ecmascript/import/lib2.mjs
@@ -0,0 +1,5 @@
+export function getEnderecoByCEP(cep) {
+
+    return fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+}
